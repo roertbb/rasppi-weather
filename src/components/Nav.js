f38aadda86d7d35eca6ae2b171c3796fa007f7ec
Robert Banaszak <robert.banaszak1997@gmail.com>
2019-01-04 23:12:04 +0100

    fetching data & rendering map

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledNav = styled.nav`
+  height: 64px;
+  width: 100vw;
+  background-color: #bada55;
+`;
+
+const Nav = () => {
+  return <StyledNav>RaspPiWeather</StyledNav>;
+};
+
+export default Nav;
